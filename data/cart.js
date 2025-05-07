2c31d27265b0e92369ed84292185a256cd926d89
Bùi Anh Tuấn <buituan29072003@gmail.com>
2025-05-07 14:10:13 +0700

    delete item from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -5,6 +5,10 @@ const cart = [{
 {
   productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity: 1
+},
+{
+  productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  quantity: 1
 }];
 
 function addToCart(productId, quantity) {
@@ -19,4 +23,11 @@ function addToCart(productId, quantity) {
   }
 }
 
-export { cart, addToCart };
+function removeFromCart (productId){
+  const productToDelete = productId;
+  const index = cart.findIndex( cartItem => cartItem.productId === productToDelete);
+  
+  cart.splice(index,1);
+}
+
+export { cart, addToCart, removeFromCart };
