e01a8913ced5ffbfb6f96ace35dfc9e8ab7c8208
Bùi Anh Tuấn <buituan29072003@gmail.com>
2025-05-23 16:22:02 +0700

    add jasmine testing framework

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -14,6 +14,6 @@ console.log('Work with 0');
 if (formatCurrency(0) === '0.00') console.log('passed');
 else console.log('failed');
 
-console.log('rounds up to the nearest cents');
+console.log('rounds up to the nearest cents'); 
 if(formatCurrency(2000.5) === '20.01') console.log('passed');
 else console.log('failed'); 
