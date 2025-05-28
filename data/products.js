cba8b5a14984db0ea1e478e4b8ee52ec13d8408d
Bùi Anh Tuấn <buituan29072003@gmail.com>
2025-05-28 17:01:35 +0700

    convert products into class

--- data/products.js
+++ data/products.js
@@ -1,8 +1,34 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId){
   let matchingItem;
   matchingItem = products.find(product => product.id === productId);
 
   return matchingItem;
+}
+
+class Product {
+  id;
+  image;
+  name;
+  raing;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  };
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
 
 export const products = [
@@ -664,4 +690,4 @@ export const products = [
       "mens"
     ]
   }
-];
+].map( productDetails =>  new Product(productDetails));
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -28,7 +28,7 @@ function renderCart() {
             ${matchingProduct.name}
           </div>
           <div class="product-price">
-            $${formatCurrency(matchingProduct.priceCents)}
+            ${matchingProduct.getPrice()}
           </div>
           <div class="product-quantity
           js-product-quantity-${matchingProduct.id}">
