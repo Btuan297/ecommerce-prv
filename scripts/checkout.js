3aab6330de84fcab411d208b5b998308858d83ea
Bùi Anh Tuấn <buituan29072003@gmail.com>
2025-06-05 15:44:44 +0700

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderCart } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/car.js'
+import '../data/backend-practice.js';
 
 //Order Summary
 renderCart();
